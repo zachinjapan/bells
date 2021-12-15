58594c20ca0b11489c403b9f40147112c1b4f514
Zach Stone <zachinjapan888@gmail.com>
2021-12-15 14:50:17 -0500

    initial commit

---
+++ src/Components/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import "../Landing/Landing.css";
+
+const Landing = () => {
+  return (
+    <div className="Landing">
+      <div className="landing-center">
+        <h1 className="title-text">Bells of the Abyss</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
