5475a3385cfe1489d8336bf10401010c81bbdbcf
Zach Stone <zachinjapan888@gmail.com>
2021-12-22 08:58:46 -0500

    embed

--- src/Components/Newsletter/Newsletter.tsx
+++ src/Components/Newsletter/Newsletter.tsx
@@ -1,10 +1,35 @@
 const Newsletter = () => {
   return (
-    <div className="scroll-div">
-      <div className="Newsletter">
-        <h1>Newsletter </h1>
-      </div>
-    </div>
+    // <div className="scroll-div">
+    //   <div className="Newsletter">
+    //     <form
+    //       action="https://gmail.us6.list-manage.com/subscribe/post?u=805a97dac23c7c5849ce2da6f&amp;id=1db2ecac0e"
+    //       method="post"
+    //       id="mc-embedded-subscribe-form"
+    //       name="mc-embedded-subscribe-form"
+    //       className="validate"
+    //       target="_blank"
+    //     >
+    //       <input
+    //         type="email"
+    //         name="EMAIL"
+    //         className="email"
+    //         id="mce-EMAIL"
+    //         placeholder="email address"
+    //         required
+    //       />
+
+    //       <input
+    //         type="submit"
+    //         value="Subscribe"
+    //         name="subscribe"
+    //         id="mc-embedded-subscribe"
+    //         className="button"
+    //       />
+    //     </form>
+    //   </div>
+    // </div>
+    <div className="Newsletter"></div>
   );
 };
 
