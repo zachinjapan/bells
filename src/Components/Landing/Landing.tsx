3303225fa40322335bb55df48567aad96d83168a
Clifford Morin <cemorin21@gmail.com>
2022-01-02 21:13:10 -0500

    added reviews page

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -1,13 +1,18 @@
-import "../Landing/Landing.css";
+import '../Landing/Landing.css';
 
 const Landing = () => {
-  return (
-    <div className="scroll-div">
-      <div className="landing-center">
-        <h1 className="title-text">Bells of the Abyss</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className="scroll-div">
+			<div className="landing-center">
+				<h1 className="title-text">Bells of the Abyss</h1>
+				<h4 className="info-text">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, sunt fugit. Ipsum vero nulla
+					consequuntur. Consectetur unde tempora perferendis iusto, officiis reiciendis. Tempore ea nisi
+					corporis ratione consequuntur nemo quas!
+				</h4>
+			</div>
+		</div>
+	);
 };
 
 export default Landing;
